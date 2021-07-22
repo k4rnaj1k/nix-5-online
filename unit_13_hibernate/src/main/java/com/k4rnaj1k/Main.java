d18853097aafde10a9b8b8ff28926418e3d43394
k4rnaj1k <vladliasota@gmail.com>
2021-07-22 18:56:53 +0300

    Appended logger, added start.bat, added shade plugin.

--- unit_13_hibernate/src/main/java/com/k4rnaj1k/Main.java
+++ unit_13_hibernate/src/main/java/com/k4rnaj1k/Main.java
@@ -4,11 +4,14 @@ import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
         Scanner s = new Scanner(System.in);
         Configuration configuration = new Configuration().configure();
         try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {
@@ -18,16 +21,22 @@ public class Main {
                 System.out.println("Initialize database?(y/n)");
                 if (s.nextLine().toLowerCase().startsWith("y")) {
                     session.beginTransaction();
+                    logger.info("Initializing the database with some default values.");
                     System.out.println("Initializing db.");
                     dbWorker.init(session);
+                    logger.info("Successfully initialized the db.");
                     session.getTransaction().commit();
                 }
                 dbWorker.printStudents();
                 System.out.println("Please input the name and the surname of the needed student.");
                 String name = s.nextLine();
                 String surname = s.nextLine();
+                logger.warn("Searching for student " + name + " " + surname + ".");
                 dbWorker.getStudent(name, surname);
+                logger.info("Successfully found the given student.");
             } catch (HibernateException e) {
+                logger.error("An exception happenned during the runtime, stopping the app and rolling back.");
+                logger.error("Exception message: " + e.getMessage());
                 session.getTransaction().rollback();
             } finally {
                 session.close();
---
+++ unit_13_hibernate/start.bat
@@ -0,0 +1,2 @@
+mvn package
+java -jar target/unit_13_hibernate-1.0.jar
