5e26f1940e02e1ea599458b7be54ec50da6fa55e
k4rnaj1k <vladliasota@gmail.com>
2021-08-14 16:52:20 +0300

    unit_15_concurrency done

---
+++ unit_15_concurrency/src/main/java/Hippodrome.java
@@ -0,0 +1,35 @@
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Hippodrome {
+    public void start() {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Input your horses number(1-10):\n>");
+        Integer choice = Integer.parseInt(s.nextLine());
+        System.out.println("3...2...1...GO");
+        CopyOnWriteArrayList<Integer> winners = new CopyOnWriteArrayList<>();
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        for (int i = 1; i <= 10; i++) {
+            service.execute(new Horse(winners, i));
+        }
+        service.shutdown();
+        System.out.println("The race has started.");
+        boolean finished = false;
+        while (!finished) {
+            try {
+                finished = service.awaitTermination(1, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        if (Objects.equals(winners.get(0), choice)) {
+            System.out.println("You have won.");
+        } else {
+            System.out.println("You have lost.");
+        }
+    }
+}
---
+++ unit_15_concurrency/src/main/java/Horse.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Horse implements Runnable {
+    private final CopyOnWriteArrayList<Integer> winners;
+    private final Integer horseNumber;
+    private Long distance = 0L;
+
+    public Horse(CopyOnWriteArrayList<Integer> winners, Integer horseNumber) {
+        this.winners = winners;
+        this.horseNumber = horseNumber;
+    }
+
+    @Override
+    public void run() {
+        Random r = new Random();
+        while (this.distance < 1000) {
+            this.distance += r.nextInt(100) + 100;
+            try {
+                Thread.sleep(r.nextInt(100) + 400);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        this.winners.add(this.horseNumber);
+        System.out.println(horseNumber + " has finished the run.");
+    }
+}
